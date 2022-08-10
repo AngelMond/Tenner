d1b367f0452597122922aea3a54d70a611896403
Pablo Garcia <pgarciag566@gmail.com>
2022-08-09 22:07:11 -0500

    SignupAndloginComponent is ready for the back end

--- client/src/components/loginAndsignup/SignupAndLogin.js
+++ client/src/components/loginAndsignup/SignupAndLogin.js
@@ -52,8 +52,56 @@ export default function SignupAndLogin(props) {
             </div>
         )
     }
-    
+
   return (
-    <div>SignupAndLogin</div>
+    <div className="Auth-form-container">
+        <form className="Auth-form">
+            <div className="Auth-form-content">
+                <h3 className="Auth-form-title">Sign Up</h3>
+                <div className="text-center">
+                    Already registered?{" "}
+                    <span className="link-primary" onClick = {changeAuthMode}>
+                        {" "}Sign In
+                    </span>
+                </div>
+
+                <div className="form-group mt-3">
+                    <label>Full Name</label>
+                    <input
+                     type = "email"
+                     className="form-control mt-1"
+                     placeholder="e.g Alejandra Peña"
+                    />
+                </div>
+
+                <div className="form-group mt-3">
+                    <label>Email adress</label>
+                    <input
+                     type = "email"
+                     className="form-control mt-1"
+                     placeholder="Email Adress"
+                    />
+                </div>
+
+                <div className="form-group mt-3">
+                    <label>Password</label>
+                    <input
+                     type = "password"
+                     className="form-control mt-1"
+                     placeholder="Password"
+                    />
+                </div>
+
+                <div className="d-grid gap-2 mt-3">
+                    <button type = "submit" className = "btn btn-primary">
+                        Submit
+                    </button>
+                </div>
+                <p className="text-center mt-2">
+                    Forgot <a href="#">password?</a>
+                </p>
+            </div>
+        </form>
+    </div>
   )
 }
