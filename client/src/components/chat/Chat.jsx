f347ebeeb38ed64c93d8df0e78dabe175f636957
Edwin0204 <edwins0204@gmail.com>
2022-08-09 22:43:09 -0500

    made the conversations section and a part of the messages section

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './chat.css'
 import Conversation from '../conversations/Conversation'
+import Message from '../message/Message'
 
 const Chat = () => {
   return (
@@ -9,11 +10,22 @@ const Chat = () => {
             <div className="chatmenuWrapper">
                 <input placeholder="Search for client" className='chatmenuInput' />
                 <Conversation />
+                <Conversation />
+                <Conversation />
+                <Conversation />
+                <Conversation />
             </div>
         </div>
         <div className="chatbox">
             <div className="chatboxWrapper">
-                Box
+                <div className="chatboxTop">
+                <Message/>
+                <Message own={true}/>
+                <Message/>
+                </div>
+                <div className="chatboxbottom">
+                    
+                </div>
             </div>
         </div>
         <div className="chatonline">
