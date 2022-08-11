9d885c233b1dbaf12215e628dce38854ffd84c6a
Chris <nunezchristian92@gmail.com>
2022-08-10 19:01:34 -0700

    added user profile and view profile js and css

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,28 @@
-import React from 'react';
+import React from "react";
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './styles/styles.css'
+import "./styles/styles.css";
 
-import Header from './components/header/Header';
-import Banner from './components/banner/Banner';
-import SignupAndLogin from './components/loginAndsignup/SignupAndLogin';
-import Chat from './components/chat/Chat';
-
-
+import Header from "./components/header/Header";
+import Banner from "./components/banner/Banner";
+import SignupAndLogin from "./components/loginAndsignup/SignupAndLogin";
+import Chat from "./components/chat/Chat";
+import UserProfile from "./components/UserProfile";
+import ViewProfile from "./components/ViewProfile";
 
 export default function App() {
   return (
     <div className="App container-full">
       <Header />
       <Banner />
-      
+
       <SignupAndLogin />
 
-    
       <main className="">
-<<<<<<< HEAD
-         <Chat/>
-=======
-        <Chat/>
->>>>>>> a2c1ed05362e1c33f0f35ee751c9b77191c21bac
-            
-            
+        <Chat />
+        <UserProfile />
+        <ViewProfile />
         {/* <Footer/> */}
       </main>
-
     </div>
-  )
+  );
 }
-
-
-
-
