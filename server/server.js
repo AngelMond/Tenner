f8b49eb922f3cc9bf8535f11cf6be03595ae5bc9
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-12 20:45:21 -0600

    importing typeDefs

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
 const { ApolloServer } = require('apollo-server-express');
+const { typeDefs } = require('./schemas/index')
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({url}) => {
+    console.log(`Your API is runing at: ${url}`);
+});
