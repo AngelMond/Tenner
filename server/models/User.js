7b328bd7cf187fd5e0d747fe88eb1411d8c748b5
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-12 20:44:13 -0600

    user model created

---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    }
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+
+
+const User = model('User', userSchema);
+
+module.exports = User;
