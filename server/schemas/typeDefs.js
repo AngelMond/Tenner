a41d46865b92bf9d6e0b00918c926844ae301bfe
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-13 15:11:32 -0600

    defining types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,15 +2,37 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
-    type User {
-        _id: ID!
-        name: String!
-        username: String!
+    type Client {
+        id: ID!
+        firstName: String!
+        lastName: String!
         age: Int!
+        username: String!
+        email: String!
+        password: String!
+        suppliers: [Supplier]
+    }
+
+    type Supplier {
+        id: ID!
+        firstName: String!
+        lastName: String!
+        age: Int!
+        username: String!
+        email: String!
+        password: String!
+        role: String!
+    }
+
+    type Images {
+        id: ID!
+        profile: String!
+        content: String!
     }
  
     type Query {
-        users: [User!]!
+        clents: [Client!]!
+        client(id: ID!): User
     }
 `;
 
