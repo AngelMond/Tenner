26f87061e9337b35fda789dbeec2795779693e42
Edwin0204 <edwins0204@gmail.com>
2022-08-13 09:41:01 -0500

    conversation middleware

--- server/index.js
+++ server/index.js
@@ -1,2 +1,10 @@
+const { application } = require("express");
 const conversationRoute = require ("./routes/conversation");
 const messageRoute = require ("./routes/messages");
+
+
+
+// middleware ///
+
+app.use ("/server/conversations", conversationRoute);
+app.use ("/server/message", messageRoute);
