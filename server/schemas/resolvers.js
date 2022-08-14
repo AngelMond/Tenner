7e3341583a61fd3aabd319cc0d13bed96b410ae0
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-14 10:11:21 -0600

    adding mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
 const clientList = require('../seeds/clientList');
+const supplierList = require('../seeds/supplierList');
+const imagesList = require('../seeds/imagesList');
 const _ = require('lodash');
 
 const resolvers = {
@@ -8,7 +10,42 @@ const resolvers = {
         },
         client: (parent, args) => {
             const id = args.id;
-            const client = _.find(clientList, {id});
+            const client = _.find(clientList, {id: Number(id)});
+
+            return client;
+        },
+        suppliers: () => {
+            return supplierList;
+        },
+        supplier: (parent, args) => {
+            const id = args.id;
+            const supplier = _.find(supplierList, {id: Number(id)});
+
+            return supplier;
+        },
+        images: () => {
+            return imagesList;
+        },
+        image: (parent, args) => {
+            const id = args.id;
+            const image = _.find(imagesList, {id: Number(id)});
+
+            return image;
+        }
+    },
+    Client: {
+        suppliers: () => {
+            return _.filter(supplierList, (supplier) => {
+               return supplier.id >= 5
+            })
+        }
+    },
+    Mutation: {
+        createClient: (parent, args) => {
+            const client = args.input;
+            const lastId = clientList[clientList.length-1].id;
+            client.id = lastId + 1;
+            clientList.push(client);
 
             return client;
         }
@@ -16,3 +53,4 @@ const resolvers = {
 }
 
 module.exports = resolvers;
+
