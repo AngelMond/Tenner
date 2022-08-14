949cd35c02d01fe7c86dfc92a863566f45dbaced
Edwin0204 <edwins0204@gmail.com>
2022-08-13 19:03:06 -0500

    made the get user

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -3,18 +3,41 @@ import './chat.css'
 import Conversation from '../conversations/Conversation'
 import Message from '../message/Message'
 import ChatOnline from '../chatOnline/ChatOnline'
+import { useContext, useState } from 'react'
+import { useEffect } from 'react'
+import {AuthContext} from ''
+import axios from 'axios'
 
-const Chat = () => {
+
+export default function Chat() {
+
+    const [conversations, setConversations] = useState([]);
+
+    {/* AuthContext te trae toda la informacion de usuario min 48*/}
+    const {user} = useContext (AuthContext)
+
+    useEffect (()=>{
+        const getConversations = async ()=>{
+            try{
+            const res = await axios.get("/conversations/"+user.id)
+            setConversations(res.data);
+            } catch(err){
+                console.log(err);
+            }
+        };
+        getConversations();
+    }, [user._id]);
+
   return (
     <div className='chat'>
         <div className="chatmenu">
             <div className="chatmenuWrapper">
                 <input placeholder="Search for client" className='chatmenuInput' />
-                <Conversation />
-                <Conversation />
-                <Conversation />
-                <Conversation />
-                <Conversation />
+                {conversations.map((c)=> (
+                <Conversation conversation={c} currentUser={user}/>
+                ))}
+               
+               
             </div>
         </div>
         <div className="chatbox">
@@ -42,4 +65,3 @@ const Chat = () => {
   )
 }
 
-export default Chat
