e8294cd89b9a8bd26e853c7435541e4fb3a4a030
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-14 10:10:57 -0600

    running graphql server in browser

--- server/server.js
+++ server/server.js
@@ -1,8 +1,26 @@
+const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 
+PORT = 3003;
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({url}) => {
-    console.log(`Your API is runing at: ${url}`);
-});
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+await server.start();
+server.applyMiddleware({ app });
+
+app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  })
+
+}
+
+startApolloServer(typeDefs, resolvers);
