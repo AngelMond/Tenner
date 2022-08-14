e15df48f375cf22d897e6eec056886bef4cc59a6
Edwin0204 <edwins0204@gmail.com>
2022-08-14 15:37:55 -0500

    live chat with socket

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -8,7 +8,9 @@ import { useEffect } from 'react'
 import {AuthContext} from ''
 import axios from 'axios'
 import { useRef } from 'react'
+import {io} from 'socket.io-client'
 
+ 
 
 export default function Chat() {
 
@@ -16,9 +18,39 @@ export default function Chat() {
     const [currentChat, setCurrentChat] = useState([null]);
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState("");
+    const [arrivalMessage, setArrivalMessage] = useState(null);
+    const [onlineUsers, setOnlineUsers] = useState([]);
     {/* AuthContext te trae toda la informacion de usuario min 48*/}
     const {user} = useContext (AuthContext);
-    const scrollRef = useRef ();
+    const scrollRef = useRef();
+    const socket = useRef();
+
+    useEffect(() => {
+        socket.current = io("ws://localhost:8900");
+        socket.current.on("getMessage", (data) => {
+            setArrivalMessage({
+              sender: data.senderId,
+              text: data.text,
+              createdAt: Date.now(),
+            });
+          });
+      }, []);
+
+      useEffect(() => {
+        arrivalMessage &&
+          currentChat?.members.includes(arrivalMessage.sender) &&
+          setMessages((prev) => [...prev, arrivalMessage]);
+      }, [arrivalMessage, currentChat]);
+
+    useEffect(() => {
+        socket.current.emit("addUser", user._id);
+        socket.current.on("getUsers", (users) => {
+          setOnlineUsers(
+            user.followings.filter((f) => users.some((u) => u.userId === f))
+          );
+        });
+      }, [user]);
+
 
     useEffect (()=>{
         const getConversations = async ()=>{
@@ -53,6 +85,19 @@ export default function Chat() {
             conversationId: currentChat._id,
         };
 
+    const receiverId = currentChat.members.find(
+        (member) => member !== user._id
+        );
+
+
+    socket.current.emit("sendMessage", {
+        senderId: user._id,
+        receiverId,
+        text: newMessage,
+        });
+
+
+
         try {
             const res = await axios.post("/messages", message);
             setMessages([...messages, res.data])
@@ -61,6 +106,9 @@ export default function Chat() {
             console.log(err);
         }
     };
+
+
+
 
     useEffect(()=>{
         scrollRef.current?.scrollIntoView({behavior:"smooth"})
