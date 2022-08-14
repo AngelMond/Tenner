c97ad3c383f2688f0c04986490d2ad518184f51a
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-14 10:11:12 -0600

    creating Schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,44 +1,44 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+// const { Schema, model } = require('mongoose');
+// const bcrypt = require('bcrypt');
 
-const userSchema = new Schema(
-  {
-    firstName: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/, 'Must use a valid email address'],
-    },
-    password: {
-      type: String,
-      required: true,
-    }
-  },
-  // set this to use virtual below
-  {
-    toJSON: {
-      virtuals: true,
-    },
-  }
-);
+// const userSchema = new Schema(
+//   {
+//     firstName: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     lastName: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     username: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       match: [/.+@.+\..+/, 'Must use a valid email address'],
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//     }
+//   },
+//   // set this to use virtual below
+//   {
+//     toJSON: {
+//       virtuals: true,
+//     },
+//   }
+// );
 
 
 
-const User = model('User', userSchema);
+// const User = model('User', userSchema);
 
-module.exports = User;
+// module.exports = User;
