3a48da19b1849d8faa06da42a1e04542dba5a356
Pablo Garcia <pgarciag566@gmail.com>
2022-08-15 13:30:10 -0500

    Added views that contain multiple components

---
+++ client/src/Views/Register.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Header from '../components/header/Header'
+import SignupAndLogin from '../components/loginAndsignup/SignupAndLogin'
+
+
+export default function Register() {
+  return (
+
+    <>
+     <Header />
+     <SignupAndLogin />
+    </>
+        
+  )
+}
