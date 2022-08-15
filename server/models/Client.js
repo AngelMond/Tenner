6a669cadab1ca708eee4a397fd5057e3c721f8bc
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-15 13:57:52 -0600

    hashing password

--- server/models/Client.js
+++ server/models/Client.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
+
 const clientSchema = new Schema(
   {
     firstName: {
@@ -47,6 +48,20 @@ const clientSchema = new Schema(
   }
 );
 
+// hash user password
+clientSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+clientSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 
 const Client = model('Client', clientSchema);
