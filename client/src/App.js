baa2bbcefdc8ba169f9a2ffd38147cc980bf194d
Pablo Garcia <pgarciag566@gmail.com>
2022-08-15 13:29:07 -0500

    added views and routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,37 +2,56 @@ import React from 'react';
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './styles/styles.css'
 
+import Home from './Views/Home';
 import Header from './components/header/Header';
-import Banner from './components/banner/Banner';
+
+
 import SignupAndLogin from './components/loginAndsignup/SignupAndLogin';
 import Chat from './components/chat/Chat';
 import UserProfile from './components/userProfile/UserProfile';
 import ViewProfile from './components/userProfile/ViewProfile';
 
+import { Route, Routes } from "react-router-dom"
+
+
+
 
 
 export default function App() {
   return (
-    <div className="App container-full">
-      {/* <Header /> */}
-      {/* <Banner /> */}
-      
-      {/* <SignupAndLogin /> */}
-
-    
-      <main className="">
-
-         {/* <Chat/> */}
-      <UserProfile />
-      <ViewProfile />
-            
-            
-        {/* <Footer/> */}
-      </main>
-
-    </div>
+    <>
+    <Header />
+    <Routes>
+      <Route path="/" element= {<Home />} />
+      <Route path ="/login" element = {<SignupAndLogin />} />
+    </Routes>
+    </>
   )
 }
+
+
+// export default function App() {
+//   return (
+//     <div className="App container-full">
+//       {/* <Header /> */}
+//       {/* <Banner /> */}
+      
+//       {/* <SignupAndLogin /> */}
+
+    
+//       <main className="">
+
+//          {/* <Chat/> */}
+//       <UserProfile />
+//       <ViewProfile />
+            
+            
+//         {/* <Footer/> */}
+//       </main>
+
+//     </div>
+//   )
+// }
 
 
 
--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
