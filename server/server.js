515381a526d643c456a24abc52ef4dafff46e211
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-14 23:00:09 -0600

    connecting mongodb local server

--- server/server.js
+++ server/server.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
+const app = express();
 
 PORT = 3003;
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers,
+  context: authMiddleware
+});
 
 
-const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
-await server.start();
-server.applyMiddleware({ app });
+  await server.start();
+  server.applyMiddleware({ app });
 
-app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  })
+    db.once('open', () => {
+      app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
 
-}
+};
 
 startApolloServer(typeDefs, resolvers);
