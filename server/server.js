6dfc5eea01468ebd07a4db0aa5130dd07feb902a
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-17 01:40:06 -0600

    new route

--- server/server.js
+++ server/server.js
@@ -4,18 +4,26 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 const app = express();
+const path = require('path');
 
-PORT = 3001;
+PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({ 
   typeDefs, 
   resolvers,
   context: authMiddleware,
 });
 
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build/')));
+}
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
