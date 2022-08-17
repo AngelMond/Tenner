199343b0eba72e71e7fff1439f5982335cc9afa9
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-17 02:32:40 -0600

    fixing class for className

--- client/src/components/banner/Banner.js
+++ client/src/components/banner/Banner.js
@@ -24,8 +24,8 @@ export default function Banner() {
                     {/* Search Input global container*/}
                     <div className='row justify-content-center mt-5'>
                         <div className='search-input-container d-flex border border-muted rounded col-auto p-0 align-items-center ps-2'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-search' viewBox='0 0 16 16'>
+                                <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
                             </svg>
                             <form className='search-form col-auto h-100'>
                                 <input onChange={handleInputChange} id='search-input' className='ps-3 pe-2 ms-2' type='search' value={search} name='' placeholder='prueba' />
