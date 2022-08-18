84910f828ddc41cb8a184664f938a9844ed9abab
AngelMond <abraham_will@hotmail.com>
2022-08-18 04:12:58 -0500

    Added handler for description input

--- client/src/components/createSupplierCard/SupplierCard.js
+++ client/src/components/createSupplierCard/SupplierCard.js
@@ -6,84 +6,28 @@ import axios from 'axios';
 
 export default function SupplierCard() {
 
-    // const [selectedFile, setSelectedFile] = useState(null);
-
-    const dragArea = document.querySelector(".drag-area");
-    const dragText = document.querySelector('.drop-text');
-    // const button = document.querySelector('#selectImageCardButton');
-    let files;
     
-    // const [inputFiles, setInputFiles] = useState();
-    
-    // //todo: agregar un envento onclick opara el button del input
-    //open the file explorer  
+    //open the file explorer when click button 
     const handleSelectFile = (e)=>{
         e.preventDefault();
         const inputFile = document.querySelector('#input-file');
         inputFile.click();
     }
 
-    // const handleInputChange = (e)=>{
-    //     files = this.files;
-    //     dragArea.classList.add("dragActive");
-    //     showFile(files);
-    //     dragArea.classList.remove('dragActive');
-    // }
+    //Handle Description Input
+    const [description, setDescription] = useState("");
+    const handleInputDescription = (e)=>{
+        setDescription(e.target.value)
+    }
 
-
-    // const showFile = (files)=>{
-    //     if(files.lenght === undefined){
-    //         processFile(files)
-    //     }else{
-    //         for (const file of files){
-    //             processFile(file)
-    //         }
-    //     }
-    // }
-
-    // const processFile = (file)=>{
-    //     const docType = file.type;
-    //     const validExtensions = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
-        
-    //     //validate file extension
-    //     if(docType.includes(validExtensions)){
-
-    //     }else{
-    //         //If image extension is not valid
-    //         alert('Sorry, your image is not a valid file extension')
-    //     }
-    // }
-
-    // // //while we drag elements it will be activated
-    // const dragOver = (e) =>{
-    //     e.preventDefault();
-
-    //     dragArea.classList.add('dragActive');
-    //     dragText.textContent = 'Drop to upload your image';
-    // }
     
-    // // //while we drag elements but noy inside de drag-area
-    // const dragLeave = (e)=>{
-    //     e.preventDefault();
-    //     dragArea.classList.remove('dragActive');
-    //     dragText.textContent = 'Drag and drop or';
-    // }
-    
-    // // //When we drop files inside the drag-area
-    // const drop = (e)=>{
-    //     e.preventDefault();
-    //     files = e.dataTransfer.files;
-    //     showFile(files);
-    //     dragArea.classList.remove('dragActive');
-    //     dragText.textContent = 'Drag and drop or';
-    // }
 
     return (
         <div className="container-fluid supplier-card-container">
             <div className="row justify-content-center p-4">
                 <div className=" p-3">
                     <form className="row justify-content-center">
-                        <div className="col-6 px-4 m-4 border rounded shadow">
+                        <div className="col-12 col-md-8 col-lg-8 px-4 m-4 border rounded shadow">
                             <h4 className="text-center p-4 fw-bold fs-2">Create your presentation card</h4>
                             <div className="input-group row g-0 mb-3">
                                 <select className="form-select" aria-label="Default select example">
@@ -99,19 +43,19 @@ export default function SupplierCard() {
                             </div>
                             <div className="input-group row g-0 mb-3">
                                 <label className="col-12 py-2 fs-5" htmlFor="description">Short description of your service</label>
-                                <textarea className="col-12 form-control descriptionInputCardSupplier" name="description" type="text" value="" placeholder="Ex: I will design your IOS/Android App..." ></textarea>
+                                <textarea onChange={handleInputDescription} className="col-12 form-control descriptionInputCardSupplier" name="description" type="text"  value={description} placeholder="Example: I will design your IOS/Android App..." ></textarea>
                             </div>
                             <div className="input-group row g-0 mb-3">
                                 <label className="col-12 py-2 fs-5" htmlFor="price">Basic</label>
-                                <input className="col-12 form-control" name="price" type="" value="" placeholder="$ 0.00" />
+                                <input onChange={handleBasicPrice} value={basicPrice} className="col-12 form-control" name="price" type="number" placeholder="$ 0.00" />
                             </div>
                             <div className="input-group row g-0 mb-3">
                                 <label className="col-12 py-2 fs-5" htmlFor="price">Standard</label>
-                                <input className="col-12 form-control" name="price" type="" value="" placeholder="$ 0.00" />
+                                <input onChange={handleStandardPrice} value={standard} className="col-12 form-control" name="price" type="number" placeholder="$ 0.00" />
                             </div>
                             <div className="input-group row g-0 mb-3">
                                 <label className="col-12 py-2 fs-5" htmlFor="price">Premium</label>
-                                <input className="col-12 form-control" name="price" type="" value="" placeholder="$ 0.00" />
+                                <input onChange={handlePremiumPrice} value={premiumPrice} className="col-12 form-control" name="price" type="number" placeholder="$ 0.00" />
                             </div>
                             <div className="input-group row g-0 mb-3 mt-5 justify-content-center">
                                 <h4 className="text-center py-2 fs-5">Add an Image</h4>
@@ -119,16 +63,16 @@ export default function SupplierCard() {
                                     <h6 className="drop-text text-center col-12 py-2 ">Drag and drop or</h6>
                                     <div className="col-12 ">
                                         <div className="d-flex justify-content-center">
-                                            <button id="selectImageCardButton" className="btn btn-primary col-4" onClick={handleSelectFile} >Select your files</button>
+                                            <button id="selectImageCardButton" className="btn btn-primary col-10 col-md-7 col-lg-2 " onClick={handleSelectFile} >Select your files</button>
                                             <input id="input-file" type="file" name="image" hidden   />
                                         </div>
                                     </div>
-                                    {/*  */}
+                        
 
                                 </div>
                                 <div className="input-group row g-0 mb-3 mt-2">
                                     <label className="col-12 py-2 fs-5" htmlFor="price">Or add your favorite image url</label>
-                                    <input className="col-12 form-control" name="price" type="" value=""  placeholder="url..." />
+                                    <input onChange={handleUrlImage} value={urlImage} className="col-12 form-control" name="price" type="" placeholder="url..." />
                                 </div>
 
                             </div>
