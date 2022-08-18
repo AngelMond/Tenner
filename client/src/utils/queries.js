72c34f1383aa9c8021082f2c44dd8e6c04b0b476
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-18 16:42:53 -0600

    adding queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,21 +1,22 @@
 import { gql } from '@apollo/client';
 
+export const ME_CLIENT = gql`
+    query MeClient {
+        meClient {
+            _id
+            username
+            email
+        
+        }
+    }
+`;
+
 export const ME_SUPPLIER = gql`
     query MeSupplier {
         meSupplier {
             _id
             username
             email
-            firstName
-            lastName
-            password
-            role
-            card {
-                _id
-                description
-                price
-                image
-            }
         }
     }
 `;
