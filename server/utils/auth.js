3f6808233b32328f0bc7ed11a428048e55730b6f
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-19 14:03:06 -0600

    updating from req.client to req.user

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -23,7 +23,7 @@ module.exports = {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
         
-      req.client = data;
+      req.user = data;
      
     } catch {
       console.log('Invalid token');
