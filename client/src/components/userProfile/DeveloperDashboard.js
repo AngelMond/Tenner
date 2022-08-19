df73051276236290fe273496b3a489485022f242
AngelMond <abraham_will@hotmail.com>
2022-08-19 17:25:28 -0500

    Added button to delete a SupplierCard

--- client/src/components/userProfile/DeveloperDashboard.js
+++ client/src/components/userProfile/DeveloperDashboard.js
@@ -1,8 +1,9 @@
 import './UserProfile.css';
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Card";
 import Container from "react-bootstrap/esm/Container";
 import { Link } from 'react-router-dom';
-import {useQuery} from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { ME_SUPPLIER } from '../../utils/queries';
 
 
@@ -16,80 +17,64 @@ import user1 from '../../images/user-1.jpg';
 export default function DeveloperDashboard() {
 
   //Get supplier data to create presentation cards
-  const {data} = useQuery(ME_SUPPLIER);
-  console.log(data.meSupplier);
+  const { data } = useQuery(ME_SUPPLIER);
+  // console.log(data.meSupplier.card.lenght);
 
-    const cardInfo = [
-        {
-          image: data.meSupplier.card[0].image,
-          username: data.meSupplier.username,
-          description: data.meSupplier.card[0].description,
-          price: data.meSupplier.card[0].basicPrice,
-        },
-        // {
-        //   image: user1,
-        //   username: "Chris Nunez",
-        //   description: "I will design your website in three days.",
-        //   price: 10,
-        // },
-        // {
-        //   image: user1,
-        //   username: "Chris Nunez",
-        //   description: "I will design your website in three days.",
-        //   price: 10,
-        // },
-        // {
-        //   image: user1,
-        //   username: "Chris Nunez",
-        //   description: "I will design your website in three days.",
-        //   price: 10,
-        // },
-        
-        
-  
-        //start here
-      ];
 
-      const renderCard = (card, index) => { 
-        return (
-          <Container className="col-auto">
-            <Card
-              style={{ width: "18rem", height: "25rem"}}
-              key={index}
-              className="shadow-lg m-1 mb-5 text-center"
-            >
-              <a>
-                <Card.Img variant="top" src={card.image} />
-              </a>
-              <Card.Body>
-                <Card.Title className='cardUsername pb-2'>{card.username}</Card.Title>
-                <p>{card.description}</p>
-                <p className='mb-0'>From</p> <h1 className='mb-0 test'>${card.price}</h1>
-              </Card.Body>
-            </Card>
-          </Container>
-        );
-      };
-    
+  const cardInfo = [
+    {
+      image: data.meSupplier.card[0].image,
+      username: data.meSupplier.username,
+      description: data.meSupplier.card[0].description,
+      price: data.meSupplier.card[0].basicPrice,
+    },
+  ];
+  // for(let i = 0; i< data.meSupplier.card.lenght ; i++){
 
+  // }
+
+  const renderCard = (card, index) => {
     return (
-        <div>
-            <div className="cardContainer text-center">
-                <h1 className="">Your Dasboard</h1>
-                <div className='form-group mt-3'>
-                    <Link to='/login/developer/description' >
-                        <button  className='btn btn-primary'
-                        type='button'>
-                            New service listing +
-                        </button>
-                    </Link>
-                </div>
-            </div>
+      <Container className="col-auto">
+        <Card
+          style={{ width: "18rem", height: "30rem" }}
+          key={index}
+          className="shadow-lg m-1 mb-5 text-center"
+        >
+          <a>
+            <Card.Img variant="top" src={card.image} />
+          </a>
+          <Card.Body>
+            <Card.Title className='cardUsername pb-2'>{card.username}</Card.Title>
+            <p>{card.description}</p>
+            <p className='mb-0'>From</p> 
+            <h1 className='mb-0 test'>${card.price}</h1>
+            <button className='mt-4 btn btn-danger'>Delete Srvice</button>
+          </Card.Body>
+        </Card>
+      </Container>
+    );
+  };
 
-            <div className="d-flex flex-wrap mt-5 container">
-                {cardInfo.map(renderCard)}
-            </div>
+
+  return (
+    <div>
+      <div className="cardContainer text-center">
+        <h1 className="">Your Dasboard</h1>
+        <div className='form-group mt-3'>
+          <Link to='/login/developer/description' >
+            <button className='btn btn-primary'
+              type='button'>
+              New service listing +
+            </button>
+          </Link>
         </div>
-    
-      );
+      </div>
+
+      <div className="d-flex flex-wrap mt-5 container">
+        {cardInfo.map(renderCard)}
+      </div>
+    </div>
+
+  );
 }
