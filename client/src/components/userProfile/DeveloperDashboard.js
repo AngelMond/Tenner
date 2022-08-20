177644dcf008351216a2426c246a4cb1e27ee7e5
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-19 21:35:20 -0600

    adding REMOVE_CARD mutation

--- client/src/components/userProfile/DeveloperDashboard.js
+++ client/src/components/userProfile/DeveloperDashboard.js
@@ -3,8 +3,10 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Card";
 import Container from "react-bootstrap/esm/Container";
 import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { ME_SUPPLIER } from '../../utils/queries';
+import { REMOVE_CARD } from '../../utils/mutations';
+import Auth from '../../utils/auth';
 
 
 
@@ -18,6 +20,9 @@ export default function DeveloperDashboard() {
 
   //Get supplier data to create presentation cards
   const { data, loading } = useQuery(ME_SUPPLIER);
+
+  // remove card from supplier
+  const [deleteCard, {error}] = useMutation(REMOVE_CARD);
   
   //Store array of cards
   let cardInfo; 
@@ -28,37 +33,36 @@ export default function DeveloperDashboard() {
   if(!loading){
     //Store supplier name
     username = data.meSupplier.username; 
-  //Store array of cards
-  cardInfo = data.meSupplier.card; 
+    //Store array of cards
+    cardInfo = data.meSupplier.card; 
 
   }else{
     return <h2>Loading...</h2>
   }
+
   
+  const handleDeleteCard = async (_id) => {
 
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-  const renderCard = (cardInfo, ix) => {
-      return (
-        <Container className="col-auto">
-          <Card
-            style={{ width: "18rem", height: "30rem" }}
-            key={ix}
-            className="shadow-lg m-1 mb-5 text-center"
-          >
-            <a>
-              <Card.Img variant="top" src={cardInfo.image} />
-            </a>
-            <Card.Body>
-              <Card.Title className='cardUsername pb-2'>{username}</Card.Title>
-              <p>{cardInfo.description}</p>
-              <p className='mb-0'>From</p> 
-              <h1 className='mb-0 test'>${cardInfo.basicPrice}</h1>
-              <button className='mt-4 btn btn-danger'>Delete Service</button>
-            </Card.Body>
-          </Card>
-        </Container>
-      );
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const {data} = await deleteCard({
+        variables: {input: {_id}}
+      });
+      
+      if (!data) {
+        throw new Error('something went wrong!');
+      }
+      window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
   };
+
 
   return (
     <div>
@@ -75,7 +79,28 @@ export default function DeveloperDashboard() {
       </div>
 
       <div className="d-flex flex-wrap mt-5 container">
-        {cardInfo.map(renderCard)}
+        {cardInfo.map((card, index)=>{
+          return (
+            <Container className="col-auto">
+              <Card
+                style={{ width: "18rem", height: "30rem" }}
+                key={index}
+                className="shadow-lg m-1 mb-5 text-center"
+              >
+                
+                  <Card.Img variant="top" src={card.image} />
+                
+                <Card.Body>
+                  <Card.Title className='cardUsername pb-2'>{username}</Card.Title>
+                  <p>{card.description}</p>
+                  <p className='mb-0'>From</p> 
+                  <h1 className='mb-0 test'>${card.basicPrice}</h1>
+                  <button className='mt-4 btn btn-danger' onClick={()=>handleDeleteCard(card._id)}>Delete Service</button>
+                </Card.Body>
+              </Card>
+            </Container>
+          );
+        })}
       </div>
     </div>
 
